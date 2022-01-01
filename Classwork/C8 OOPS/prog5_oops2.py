b11a959f48b58222eb4e2997e75eb3d7f12f6b6b
TheSagarPatil <thesagarpatil@Live.com>
2022-01-01 21:51:59 +0530

    Create prog5_oops2.py

---
+++ Classwork/C8 OOPS/prog5_oops2.py
@@ -0,0 +1,30 @@
+class Demo:
+    A = 10
+    B = 20
+
+    def __init__(self):
+        self.c = 30
+        self.d = 40
+
+def printValuesOfInstance(INST, CLASS):
+    className = INST.__class__.__name__
+    print('class :', className, '====')
+    print('value of A ' , CLASS.A)
+    print('value of B ' , CLASS.B)
+    print('value of c ' , INST.c)
+    print('value of d ' , INST.d)
+    print(locals())
+    
+def main():
+    
+    d = Demo()
+    d2 = Demo()
+    printValuesOfInstance(d, Demo)
+    printValuesOfInstance(d2, Demo)
+    d2.c +=1
+    Demo.A +=1
+    printValuesOfInstance(d, Demo)
+    printValuesOfInstance(d2, Demo)
+
+if __name__ == '__main__':
+    main()
